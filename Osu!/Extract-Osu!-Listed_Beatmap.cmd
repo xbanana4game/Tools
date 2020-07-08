773210a228597ef10126b7f802915e4d631ff08f
mspylozic <mspylozic@gmail.com>
2020-07-08 13:22:42 +0900

    Modified   Osu!/Backup-Osu!-settings.cmd Added      Osu!/Extract-Osu!-Listed_Beatmap.cmd Modified   Osu!/Make-Osu!-Favorite.cmd Added      Osu!/Replace 7z Listfile.mac Added      Osu!/Step1 - Grep Beatmap Pack.mac Added      Osu!/Step2 - Replace GrepResult.mac Added      Osu!/osu! Stream Speed Benchmark.cmd

---
+++ Osu!/Extract-Osu!-Listed_Beatmap.cmd
@@ -0,0 +1,36 @@
+@ECHO OFF
+CALL %USERPROFILE%\Settings.cmd
+
+REM ----------------------------------------------------------------------
+REM Beatmap ディレクトリーチェック
+REM ----------------------------------------------------------------------
+ECHO %BEATMAP_DIR%
+IF EXIST "%BEATMAP_DIR%" (
+  CD /D "%BEATMAP_DIR%"
+) ELSE (
+  SET /P ERROR=フォルダ%BEATMAP_DIR%が存在しません
+  EXIT
+)
+
+
+REM ----------------------------------------------------------------------
+REM 
+REM ----------------------------------------------------------------------
+EXPLORER "%OSU_DIR%\Logs"
+"%OSU_DIR%\Logs\listfile.txt"
+SET /P CMD_EXECUTE="Songsに追加しますか? 1/0 -> "
+
+IF 1 EQU %CMD_EXECUTE% (
+  FOR /D %%i IN ("%BEATMAP_DIR%\*") DO (
+    CD /D "%%i"
+    FOR %%j IN ("*rar") DO (
+      7z e "%%i\%%j" -o%OSU_SONGS_DIR%  -i@"%OSU_DIR%\Logs\listfile.txt"
+    )
+    FOR %%j IN ("*7z") DO (
+      7z e "%%i\%%j" -o%OSU_SONGS_DIR%  -i@"%OSU_DIR%\Logs\listfile.txt"
+    )
+  )
+  EXPLORER %OSU_SONGS_DIR%
+)
+
+
--- Osu!/Make-Osu!-Favorite.cmd
+++ Osu!/Make-Osu!-Favorite.cmd
@@ -5,15 +5,14 @@ REM ----------------------------------------------------------------------
 REM BEATMAP_FAVORITEディレクトリーチェック
 REM ----------------------------------------------------------------------
 CALL :CheckDirectory "%BEATMAP_FAVORITE%"
-CALL :CheckDirectory "%BEATMAP_DIR%"
 
 REM ----------------------------------------------------------------------
 REM Archive
 REM ----------------------------------------------------------------------
 FOR /D %%i IN ("%BEATMAP_FAVORITE%\*") DO (
-  7z a -t7z -sdel "%BEATMAP_DIR%\%%~ni.7z" "%%i\*.osz" -mx0
+  7z a -t7z -sdel "%BEATMAP_FAVORITE%\%%~ni.7z" "%%i\*.osz" -mx0
 )
-EXPLORER "%BEATMAP_DIR%"
+EXPLORER "%BEATMAP_FAVORITE%"
 
 EXIT
 
@@ -21,11 +20,11 @@ REM ----------------------------------------------------------------------
 REM CALL :CheckDirectory [ディレクトリー]
 REM ----------------------------------------------------------------------
 :CheckDirectory
-IF EXIST %1 (
-  ECHO フォルダ%1が存在
-) ELSE (
-  SET /P ERR=フォルダ%1が存在しません
-  EXIT
-)
-EXIT /B 0
+	IF EXIST %1 (
+		ECHO フォルダ%1が存在
+	) ELSE (
+		SET /P ERR=フォルダ%1が存在しません
+		EXIT
+	)
+	EXIT /B 0
 
