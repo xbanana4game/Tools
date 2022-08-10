89adb6a75990805a5c79552d881cb9165658766d
mspylozic <mspylozic@gmail.com>
2022-08-10 10:42:28 +0900

    Modified   Archive-Tools/Archive_Directory.cmd Modified   Setup/MoveFiles.cmd.txt

--- Archive-Tools/Archive_Directory.cmd
+++ Archive-Tools/Archive_Directory.cmd
@@ -12,20 +12,20 @@ REM
 REM                                Main
 REM
 REM ======================================================================
-IF NOT DEFINED OUTPUT_DIR (SET /P OUTPUT_DIR="SET OUTPUT_DIR(%DOWNLOADS_DIR%)=")
-IF NOT DEFINED OUTPUT_DIR (SET OUTPUT_DIR=%DOWNLOADS_DIR%)
+IF NOT DEFINED ARCHIVE_OUTPUT_DIR (SET /P ARCHIVE_OUTPUT_DIR="SET ARCHIVE_OUTPUT_DIR(%DOWNLOADS_DIR%)=")
+IF NOT DEFINED ARCHIVE_OUTPUT_DIR (SET ARCHIVE_OUTPUT_DIR=%DOWNLOADS_DIR%)
 
 IF NOT "%DOWNLOADS_PASSWORD%"=="" SET ARCHIVE_OPT_PW=-p%DOWNLOADS_PASSWORD% -mhe
 
 FOR %%i in (%*) DO (
 	ECHO %%i
 	REM VD_YYYYMMDD
-	7z a -t7z -sdel %ARCHIVE_OPT_PW% "%OUTPUT_DIR%\%%~nxi.7z" ""%%i"" -xr!.ts -mx=5 -v1g
+	7z a -t7z -sdel %ARCHIVE_OPT_PW% "%ARCHIVE_OUTPUT_DIR%\%%~nxi.7z" ""%%i"" -xr!.ts -mx=5 -v1g
 	REM 7z a -t7z -sdel "%%~nxi.7z" ""%%i"" -mx=0
-	IF EXIST "%OUTPUT_DIR%\%%~nxi.7z.001" 7z l %ARCHIVE_OPT_PW% "%OUTPUT_DIR%\%%~nxi.7z.001" >"%OUTPUT_DIR%\%%~nxi.7z.txt"
-	IF EXIST "%OUTPUT_DIR%\%%~nxi.7z" 7z l %ARCHIVE_OPT_PW% "%OUTPUT_DIR%\%%~nxi.7z" >"%OUTPUT_DIR%\%%~nxi.7z.txt"
-	TYPE "%OUTPUT_DIR%\%%~nxi.7z.txt"
+	IF EXIST "%ARCHIVE_OUTPUT_DIR%\%%~nxi.7z.001" 7z l %ARCHIVE_OPT_PW% "%ARCHIVE_OUTPUT_DIR%\%%~nxi.7z.001" >"%ARCHIVE_OUTPUT_DIR%\%%~nxi.7z.txt"
+	IF EXIST "%ARCHIVE_OUTPUT_DIR%\%%~nxi.7z" 7z l %ARCHIVE_OPT_PW% "%ARCHIVE_OUTPUT_DIR%\%%~nxi.7z" >"%ARCHIVE_OUTPUT_DIR%\%%~nxi.7z.txt"
+	TYPE "%ARCHIVE_OUTPUT_DIR%\%%~nxi.7z.txt"
 )
-PAUSE
+REM PAUSE
 EXIT
 
