6e993ecef45d284edaab8e7886b5ff14b30a4d49
mspylozic <mspylozic@gmail.com>
2022-09-06 02:18:04 +0900

    Modified   Archive-Tools/Archive_Directory.cmd Modified   Archive-Tools/Extract.cmd

--- Archive-Tools/Extract.cmd
+++ Archive-Tools/Extract.cmd
@@ -71,11 +71,11 @@ REM ======================================================================
 			7z l -p%DOWNLOADS_PASSWORD% %%j>%EXTRACT_TARGET_DIR%\%%~ni\%%~nj.txt
 		)
 		FOR %%j IN ("%ARCHIVE_DIR%\%%~ni\DL_*.7z.001") DO (
-			7z x -p%DOWNLOADS_PASSWORD% %%j -o%EXTRACT_TARGET_DIR%\%%~ni -aot %EXTRACT_EXT% -r
+			7z x -p%DOWNLOADS_PASSWORD% %%j -o%EXTRACT_TARGET_DIR%\%%~ni -aot %EXTRACT_EXT% -r %IGNORE_OPT%
 			7z l -p%DOWNLOADS_PASSWORD% %%j>%EXTRACT_TARGET_DIR%\%%~ni\%%~nj.txt
 		)
 		FOR %%j IN ("%ARCHIVE_DIR%\%%~ni\VD_*.7z.001") DO (
-			7z x -p%DOWNLOADS_PASSWORD% %%j -o%EXTRACT_TARGET_DIR%\%%~ni -aot %EXTRACT_EXT% -r
+			7z x -p%DOWNLOADS_PASSWORD% %%j -o%EXTRACT_TARGET_DIR%\%%~ni -aot %EXTRACT_EXT% -r %IGNORE_OPT%
 			7z l -p%DOWNLOADS_PASSWORD% %%j>%EXTRACT_TARGET_DIR%\%%~nj.txt
 		)
 		IF %MOVE_STORE_FLG%==1 (
@@ -90,6 +90,6 @@ REM ======================================================================
 	SET EXTRACT_FILE=%1
 	SET EXTRACT_EXT=*
 	SET OUTPUT_DIR=%USERPROFILE%\Desktop
-	7z x -p%DOWNLOADS_PASSWORD% %EXTRACT_FILE% -o%OUTPUT_DIR%\* %EXTRACT_EXT% -r
+	7z x -p%DOWNLOADS_PASSWORD% %EXTRACT_FILE% -o%OUTPUT_DIR%\* %EXTRACT_EXT% -r %IGNORE_OPT%
 	EXIT /B
 
