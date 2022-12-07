dca7258fd9f57b2f53d170d7e5e2072260754d29
mspylozic <mspylozic@gmail.com>
2022-12-07 14:51:36 +0900

    Modified   Archive-Tools/Archive_Videos.cmd Modified   Setup/Rename Rule.txt

--- Archive-Tools/Archive_Videos.cmd
+++ Archive-Tools/Archive_Videos.cmd
@@ -15,13 +15,13 @@ REM
 REM ======================================================================
 REM SET OUTPUT_DIRECTORY=%USERPROFILE%\Videos
 SET OUTPUT_DIRECTORY=.
-SET OUTPUT_FILE_EXT=zip.mp4
-IF NOT EXIST MD %OUTPUT_DIRECTORY%
+SET OUTPUT_FILE_EXT=zip
+REM IF NOT EXIST MD %OUTPUT_DIRECTORY%
 FOR %%i in (%*) DO (
 	ECHO %%i
 	IF NOT EXIST "%OUTPUT_DIRECTORY%\%%~nxi.%OUTPUT_FILE_EXT%" (
-		7z a -tzip -sdel "%OUTPUT_DIRECTORY%\%%~nxi.%OUTPUT_FILE_EXT%" ""%%i\*"" -xr!*.db -xr!*.ts -xr!.DS_Store -mx=0 -mtc=off
-		7z l "%OUTPUT_DIRECTORY%\%%~nxi.%OUTPUT_FILE_EXT%">"%%~nxi.%OUTPUT_FILE_EXT%.txt"
+		7z a -tzip -sdel "%OUTPUT_DIRECTORY%\%%~nxi.%OUTPUT_FILE_EXT%" ""%%i\*"" -xr!.@__thumb -xr!*.db -xr!*.ts -xr!.DS_Store -mx=0 -mtc=off
+		REM 7z l "%OUTPUT_DIRECTORY%\%%~nxi.%OUTPUT_FILE_EXT%">"%%~nxi.%OUTPUT_FILE_EXT%.txt"
 	)
 )
 EXIT
