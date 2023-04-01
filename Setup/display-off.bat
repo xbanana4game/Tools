328ede15e527154b322f7a8509357513d8c4d734
mspylozic <mspylozic@gmail.com>
2023-04-02 00:45:59 +0900

    Added      Setup/display-off.bat Modified   Setup/MoveFiles.cmd.txt

---
+++ Setup/display-off.bat
@@ -0,0 +1,2 @@
+ping -n 3 localhost
+powershell (Add-Type '[DllImport(\"user32.dll\")]^public static extern int SendMessage(int hWnd, int hMsg, int wParam, int lParam);' -Name a -Pas)::SendMessage(-1,0x0112,0xF170,2)
