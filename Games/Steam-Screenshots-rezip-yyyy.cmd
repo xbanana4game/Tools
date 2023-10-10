d6e8f8c177d6538fcba9960aea78c7ce323f4e2e
mspylozic <mspylozic@gmail.com>
2023-10-10 11:55:00 +0900

    Deleted    Setup/testMoveFiles.cmd Modified   Games/Steam-Screenshots-rezip-yyyy.cmd Modified   Sakura/m3u2wpl.ppa

--- Games/Steam-Screenshots-rezip-yyyy.cmd
+++ Games/Steam-Screenshots-rezip-yyyy.cmd
@@ -10,8 +10,10 @@ REM ---------- Steam-Screenshots-rezip-yyyy.cmd(SettingsOptions.cmd) ----------
 SET yyyy=2023
 REM IF NOT DEFINED DRIVE_LETTER (SET DRIVE_LETTER=%CD:~0,1%)
 REM SET SS_BASE_DIR=%DRIVE_LETTER%:\ARCHIVE%yyyy%\Downloads\%yyyy%
-SET SS_OUTPUT_DIR=%DESKTOP_DIR%
+SET SS_OUTPUT_DIR=%DESKTOP_DIR%\Screenshots
+REM SET SS_OUTPUT_DIR=%DRIVE_LETTER%:\ARCHIVE%yyyy%\Downloads\Screenshots
 SET SS_WORK_DIR=%DESKTOP_DIR%\Steam-SS-Work
+SET SS_STORE_DIR=%DRIVE_LETTER%:\ARCHIVE%yyyy%\Downloads\Screenshots-Store
 
 
 REM ======================================================================
@@ -26,6 +28,7 @@ IF NOT DEFINED SS_BASE_DIR (SET /P SS_BASE_DIR="SET SS_BASE_DIR(E:\ARCHIVE%yyyy%
 IF NOT DEFINED SS_BASE_DIR (SET SS_BASE_DIR=E:\ARCHIVE%yyyy%\Downloads\%yyyy%)
 CD /D %SS_BASE_DIR%
 MD %SS_WORK_DIR%
+IF NOT EXIST %SS_OUTPUT_DIR% MD %SS_OUTPUT_DIR%
 
 ECHO %SS_BASE_DIR%
 EXPLORER %SS_BASE_DIR%
@@ -34,8 +37,10 @@ PAUSE
 FOR /F "tokens=1,2* delims=," %%i IN (%TOOLS_DIR%\Games\steam_screenshots.txt) DO (
 	ECHO SEARCH %SS_BASE_DIR%\%%i-Screenshots-%yyyy%*.zip
 	MD %SS_WORK_DIR%\%%i
+	IF DEFINED SS_STORE_DIR MD %SS_STORE_DIR%
 	FOR /R %%j IN ("%%i-Screenshots-%yyyy%*.zip") DO (
 		7z x %%j -o"%SS_WORK_DIR%\%%i" -xr!thumbnails
+		IF DEFINED SS_STORE_DIR MOVE %%j %SS_STORE_DIR%
 	)
 	RMDIR %SS_WORK_DIR%\%%i
 	IF EXIST %SS_WORK_DIR%\%%i 7z -tzip -sdel a "%SS_OUTPUT_DIR%\%%i-Screenshots-%yyyy%.zip" %SS_WORK_DIR%\%%i\* -mx=0
