ccadbe5dabfa415ce74b3dba75fcce01e414b269
mspylozic <mspylozic@gmail.com>
2023-12-20 13:01:29 +0900

    Modified   Template/yt-dlp.test.conf Modified   Template/ytdlp-test.cmd

--- Template/ytdlp-test.cmd
+++ Template/ytdlp-test.cmd
@@ -18,10 +18,13 @@ REM ======================================================================
 REM yt-dlp.exe --help >yt-dlp.txt
 REM yt-dlp.exe --version
 COPY yt-dlp.test.conf %VIDEOS_DIR%\yt-dlp.conf
+REM IF NOT EXIST "%VIDEOS_DIR%\yt-dlp.conf" (COPY yt-dlp.test.conf %VIDEOS_DIR%\yt-dlp.conf)
 REM NOTEPAD yt-dlp.conf
 NOTEPAD %VIDEOS_DIR%\dlurl.txt
 CD %VIDEOS_DIR%
 yt-dlp.exe %YTDLP_UPDATE_OPT%
 MOVE %VIDEOS_DIR%\dlurl.txt "%VIDEOS_DIR%\dlurl-%yyyy%%mm%%dd%_%hh%%mn%.txt"
 REM pause
+
+EXPLORER %VIDEOS_DIR%\yt-dlp
 
