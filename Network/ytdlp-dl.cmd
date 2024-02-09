77dd1312792989653223f41f565fa27439417c92
mspylozic <mspylozic@gmail.com>
2024-02-09 13:09:57 +0900

    Added      Sakura/vlcjpg-chapter.ppa Modified   Network/yt-dlp.default.conf Modified   Network/ytdlp-dl.cmd Modified   Setup/Mp3tagSettings/data/actions/category.txt

--- Network/ytdlp-dl.cmd
+++ Network/ytdlp-dl.cmd
@@ -10,6 +10,7 @@ REM ---------- ytdlp-dl.cmd(SettingsOptions.cmd) ----------
 REM SET YTDLP_UPDATE_OPT=-U
 REM SET YTDLP_PROFILE=default
 REM SET SHUTDOWN_FLG=0
+REM SET VIDEO_OUTPUT_DIR=%USERPROFILE%\Videos\yt-dlp
 
 
 REM ======================================================================
@@ -20,6 +21,7 @@ REM ======================================================================
 IF NOT DEFINED SHUTDOWN_FLG (SET SHUTDOWN_FLG=0)
 IF 1 EQU %SHUTDOWN_FLG% SET /P SHUTDOWN_FLG2="Shutdown? 1:YES 0:NO -> "
 IF NOT DEFINED YTDLP_PROFILE (SET YTDLP_PROFILE=default)
+IF NOT DEFINED VIDEO_OUTPUT_DIR SET VIDEO_OUTPUT_DIR=%USERPROFILE%\Videos\yt-dlp
 
 REM yt-dlp.exe --help >yt-dlp.txt
 REM yt-dlp.exe --version
@@ -36,13 +38,14 @@ REM NOTEPAD yt-dlp.conf
 ECHO;>>%VIDEOS_DIR%\dlurl.txt
 NOTEPAD %VIDEOS_DIR%\dlurl.txt
 CD %VIDEOS_DIR%
-yt-dlp.exe -a dlurl.txt %YTDLP_UPDATE_OPT%
+REM IF DEFINED (yt-dlp.exe %YTDLP_UPDATE_OPT%) ELSE (yt-dlp.exe -a dlurl.txt)
+yt-dlp.exe -a dlurl.txt
 TYPE %VIDEOS_DIR%\dlurl.txt >>"%VIDEOS_DIR%\dlurl.log"
 ECHO;>>"%VIDEOS_DIR%\dlurl.log"
 DEL %VIDEOS_DIR%\dlurl.txt
 REM PAUSE
 
-REM EXPLORER %VIDEOS_DIR%\yt-dlp
+EXPLORER %VIDEO_OUTPUT_DIR%
 REM Mp3tag.exe /fp:"%VIDEOS_DIR%\yt-dlp"
 IF 1 EQU %SHUTDOWN_FLG2% (SHUTDOWN /S /T 3)
 
