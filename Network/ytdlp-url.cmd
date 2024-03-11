7e1801a265a00d246d7ea6af87c4e89be5009be3
mspylozic <mspylozic@gmail.com>
2024-03-11 13:50:16 +0900

    Added      Network/ytdlp-url.cmd Modified   Network/local-copy.cmd

---
+++ Network/ytdlp-url.cmd
@@ -0,0 +1,36 @@
+@ECHO OFF
+REM ======================================================================
+REM
+REM                                Settings
+REM
+REM ======================================================================
+IF NOT EXIST %USERPROFILE%\.Tools\Settings.cmd (EXIT)
+CALL %USERPROFILE%\.Tools\Settings.cmd
+REM ---------- ytdlp-dl.cmd(SettingsOptions.cmd) ----------
+REM SET YTDLP_UPDATE_OPT=-v -F
+
+
+REM ======================================================================
+REM
+REM                                Main
+REM
+REM ======================================================================
+IF NOT DEFINED YTDLP_PROFILE (SET YTDLP_PROFILE=default)
+IF NOT DEFINED VIDEO_OUTPUT_DIR SET VIDEO_OUTPUT_DIR=%USERPROFILE%\Videos\yt-dlp
+
+IF NOT "%1"=="" (
+	SET YTDLP_PROFILE_FILE=%~1
+	COPY %~1 %VIDEOS_DIR%\yt-dlp.conf
+) ELSE (
+	SET YTDLP_PROFILE_FILE=yt-dlp.%YTDLP_PROFILE%.conf
+)
+IF NOT EXIST %VIDEOS_DIR%\yt-dlp.conf (COPY %YTDLP_PROFILE_FILE% %VIDEOS_DIR%\yt-dlp.conf)
+CD %VIDEOS_DIR%
+
+:DL_START
+SET /P DOWNLOAD_URL="URL: "
+yt-dlp.exe %YTDLP_UPDATE_OPT% %DOWNLOAD_URL%
+EXPLORER %VIDEO_OUTPUT_DIR%
+GOTO :DL_START
+
+EXIT
