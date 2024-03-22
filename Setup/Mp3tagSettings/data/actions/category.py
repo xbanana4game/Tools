070d3e52c3a3d8b25836c593f2e76baabdd5fc9f
mspylozic <mspylozic@gmail.com>
2024-03-23 00:39:37 +0900

    Modified   Games/Rename-captures.cmd Modified   Games/captures.txt Modified   Setup/Mp3tagSettings/data/actions/category.py Modified   Setup/Mp3tagSettings/data/actions/youtube-album.py Modified   Setup/Rename Rule.txt

--- Setup/Mp3tagSettings/data/actions/category.py
+++ Setup/Mp3tagSettings/data/actions/category.py
@@ -13,7 +13,7 @@ for data in fr:
     mta_file=mta.mtafile(mta_filename)
     mta_file.format_value('ALBUM',album)
     mta_file.remove_field('ALBUMARTIST')
-    mta_file.format_value('_FILENAME','$if($grtr($strstr(%_folderpath%,\\\\$getenv(NASDOMAIN)),0),\\\\$getenv(NASDOMAIN)\\Multimedia\\Videos\\,$if($strcmp($left(%_folderpath%,3),C:\\),C:\\Users\\$getenv(USERNAME)\\Downloads\\,$left(%_folderpath%,3)Videos\\))youtube.com\\$if($isdigit(%_covers%),,no_cover\\)$if($strcmp(%albumartist%,youtube.com),$replace(_%genre%_,&,and)\\,$if2(%album%,_work)\\)\\$if($and(%year%,%comment%),%year% %comment%,%_FILENAME%)')
+    mta_file.format_value('_FILENAME','$if($grtr($strstr(%_folderpath%,\\\\$getenv(NASDOMAIN)),0),\\\\$getenv(NASDOMAIN)\\Multimedia\\Videos\\,$if($strcmp($left(%_folderpath%,3),C:\\),C:\\Users\\$getenv(USERNAME)\\Downloads\\,$left(%_folderpath%,3)Videos\\))youtube.com\\$if($isdigit(%_covers%),,no_cover\\) $if($strcmp(%albumartist%,channel),__channel\\,) $if($strcmp(%albumartist%,youtube.com),$replace(_%album%_,&,and)\\,$if2(%album%,_work)\\)\\$if($and(%year%,%comment%),%year% %comment%,%_FILENAME%)')
     mta_file.close()
 
 fr.close()
--- Setup/Mp3tagSettings/data/actions/youtube-album.py
+++ Setup/Mp3tagSettings/data/actions/youtube-album.py
@@ -2,21 +2,20 @@ import mta
 import os
 
 fc = open('category.txt', 'r', encoding='UTF-8')
+mta_file=mta.mtafile('youtube-album.mta')
 for data in fc:
     profile=data.rstrip('\n')
     is_file = os.path.isfile('youtube-album('+profile+').txt')
     if not is_file:
         continue
     fr = open('youtube-album('+profile+').txt', 'r', encoding='UTF-8')
-    mta_filename='youtube-album('+profile+').mta'
-    mta_file=mta.mtafile(mta_filename)
 
     for data in fr:
         artist=data.rstrip('\n')
         mta_file.format_value('ALBUM','$if($grtr($strstr(%artist%,'+artist+'),0),'+profile+',%album%)')
 
     fr.close()
-    mta_file.format_value('_FILENAME','$if($grtr($strstr(%_folderpath%,\\\\$getenv(NASDOMAIN)),0),\\\\$getenv(NASDOMAIN)\\Multimedia\\Videos\\,$if($strcmp($left(%_folderpath%,3),C:\\),C:\\Users\\$getenv(USERNAME)\\Downloads\\,$left(%_folderpath%,3)Videos\\))youtube.com\\$if($isdigit(%_covers%),,no_cover\\)$if($strcmp(%albumartist%,youtube.com),$replace(_%genre%_,&,and)\\,$if2(%album%,_work)\\)\\$if($and(%year%,%comment%),%year% %comment%,%_FILENAME%)')
-    mta_file.close()
 
+mta_file.format_value('_FILENAME','$if($grtr($strstr(%_folderpath%,\\\\$getenv(NASDOMAIN)),0),\\\\$getenv(NASDOMAIN)\\Multimedia\\Videos\\,$if($strcmp($left(%_folderpath%,3),C:\\),C:\\Users\\$getenv(USERNAME)\\Downloads\\,$left(%_folderpath%,3)Videos\\))youtube.com\\$if($isdigit(%_covers%),,no_cover\\)$if($strcmp(%albumartist%,youtube.com),$replace(_%genre%_,&,and)\\,$if2(%album%,_work)\\)\\$if($and(%year%,%comment%),%year% %comment%,%_FILENAME%)')
+mta_file.close()
 fc.close()
