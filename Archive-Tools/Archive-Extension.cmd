6bb10be667f724d6803738d478e5d4db1c92f1b1
mspylozic <mspylozic@gmail.com>
2025-01-03 09:22:06 +0900

    Modified   Archive-Tools/Archive-Extension.cmd Modified   Archive-Tools/Archive-Tool.cmd Modified   Archive-Tools/Archive_Directory-winrar.cmd Modified   Network/local-copy.cmd

--- Archive-Tools/Archive-Extension.cmd
+++ Archive-Tools/Archive-Extension.cmd
@@ -11,11 +11,18 @@ REM
 REM                                Main
 REM
 REM ======================================================================
-IF NOT DEFINED SDEL_FLG (SET /P SDEL_FLG="Enter SDEL_FLG 1/0(default:0): ")
-IF "%SDEL_FLG%"=="1" (SET SDEL_OPT=-sdel)
+CHOICE /C 01 /M "SDEL_FLG :"
+IF %ERRORLEVEL% EQU 1 (
+	REM SDEL_FLG :0
+	SET SDEL_OPT=
+) ELSE IF %ERRORLEVEL% EQU 2 (
+	REM SDEL_FLG :1
+	SET SDEL_OPT=-sdel
+)
 
 IF "%~x1" == ".7zext" (
 	SET OUTPUT_DIR_NAME=%~n1-files
+	SET TARGET_DIR=%~n1
 	CALL :EXECUTE_ARCHIVE_EXT_SETTINGS_FROM_FILE %~1
 ) ELSE (
 	SET OUTPUT_DIR_NAME=files
@@ -51,11 +58,13 @@ REM ======================================================================
 
 :EXECUTE_ARCHIVE_EXT_SETTINGS_FROM_FILE
 	SET EXT_LIST_FILE=%1
+	CD %TARGET_DIR%
+	SET OUTPUT_DIR=%OUTPUT_DIR_NAME%
 	FOR /F "skip=1 tokens=1,2 delims=;" %%C IN (%EXT_LIST_FILE%) DO (
-		IF NOT EXIST %OUTPUT_DIR_NAME% MD %OUTPUT_DIR_NAME%
-		7z a -tzip %SDEL_OPT% %OUTPUT_DIR_NAME%\%%C %%D -r -xr!Archive-Extension.cmd -xr!%%C -xr!%%C.txt -xr!.ts -mx=0
-		7z l %OUTPUT_DIR_NAME%\%%C >%OUTPUT_DIR_NAME%\%%C.txt
-		TYPE %OUTPUT_DIR_NAME%\%%C.txt
+		IF NOT EXIST %OUTPUT_DIR% MD %OUTPUT_DIR%
+		7z a -tzip %SDEL_OPT% %OUTPUT_DIR%\%%C %%D -r -xr!Archive-Extension.cmd -x!%OUTPUT_DIR_NAME% -xr!%%C -xr!%%C.txt -xr!.ts -mx=0
+		7z l %OUTPUT_DIR%\%%C >%OUTPUT_DIR%\%%C.txt
+		TYPE %OUTPUT_DIR%\%%C.txt
 	)
 	EXIT /B
 
