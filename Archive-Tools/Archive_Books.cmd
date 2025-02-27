eada07ab1bfecf93f5bad7e7803f264c42109ae4
mspylozic <mspylozic@gmail.com>
2025-02-27 13:39:33 +0900

    Modified   Archive-Tools/Archive_Books.cmd

--- Archive-Tools/Archive_Books.cmd
+++ Archive-Tools/Archive_Books.cmd
@@ -25,7 +25,7 @@ FOR %%i in (%*) DO (
 		7z a -tzip -sdel "%BOOKS_ZIP_DIR%\%%~nxi.zip" ""%%i\*"" -xr!*.db -xr!*.dat -xr!*.url -xr!.DS_Store -mx=0 -mtc=off
 		7z l "%BOOKS_ZIP_DIR%\%%~nxi.zip">"%%~nxi.zip.txt"
 	)
-	books_rename.py "%BOOKS_ZIP_DIR%\%%~nxi.zip"
+	books_rename.py "%%i.zip"
 	RMDIR %%i
 )
 EXIT
