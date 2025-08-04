96a2c429d707e3cd0a70c63dd23a8c3d001c96b9
mspylozic <mspylozic@gmail.com>
2025-08-04 12:15:27 +0900

    Added      Games/Epic Game vhdx.cmd

---
+++ Games/Epic Game vhdx.cmd
@@ -0,0 +1,48 @@
+@ECHO OFF
+REM ======================================================================
+REM
+REM                                Settings
+REM
+REM ======================================================================
+IF NOT EXIST %USERPROFILE%\.Tools\Settings.cmd (EXIT)
+CALL %USERPROFILE%\.Tools\Settings.cmd
+SETLOCAL ENABLEDELAYEDEXPANSION
+SET DRIVE_LETTER_FILE=%CD:~0,2%
+SET DRIVE_LETTER_CMD=%~d0
+
+REM ---------- Template.cmd(SettingsOptions.cmd) ----------
+
+
+REM ======================================================================
+REM
+REM                                Main
+REM
+REM ======================================================================
+SET LAUNCHER_NAME=Epic Games
+7z x "%CLOUD_DRIVE%\Cloud-Tools\Games\%LAUNCHER_NAME% - X.zip"
+IF NOT EXIST "%LAUNCHER_NAME% - X.vhdx" EXIT
+SET /P GAME_NAME="GAME NAME:"
+IF EXIST "%LAUNCHER_NAME% - %GAME_NAME%.vhdx" EXIT
+ECHO %LAUNCHER_NAME% - %GAME_NAME%
+MOVE "Epic Games - X.vhdx" "Epic Games - %GAME_NAME%.vhdx"
+SET GAME_INSTALL_DIR="C:\Games\Epic Games\%GAME_NAME%"
+REM MOUNTVOL %GAME_INSTALL_DIR% /D
+MD %GAME_INSTALL_DIR%
+ECHO %GAME_INSTALL_DIR%
+REM MOUNTVOL /L
+PAUSE
+EXIT
+
+
+REM ======================================================================
+REM
+REM                                Function
+REM
+REM ======================================================================
+:SUCCESS
+	EXIT /B
+	
+:ERROR
+	EXIT /B 1
+
+
