225c9c8057af55a548ad6ee08b16e031b87ac840
mspylozic <mspylozic@gmail.com>
2026-06-23 07:10:27 +0900

    Modified   Archive-Tools/Archive-Extension-Sample.7zext Modified   Archive-Tools/Archive-Extension.cmd

--- Archive-Tools/Archive-Extension.cmd
+++ Archive-Tools/Archive-Extension.cmd
@@ -51,6 +51,7 @@ REM ======================================================================
 	IF "%ARCHIVE_PREFIX%"=="" SET OUTPUT_FILENAME=%ARCHIVE_EXT%-files
 
 	IF NOT EXIST %OUTPUT_DIR_NAME% MD %OUTPUT_DIR_NAME%
+	ECHO 7z a -tzip %SDEL_OPT% %OUTPUT_DIR_NAME%\%OUTPUT_FILENAME%.zip  %ARCHIVE_PREFIX%*.%ARCHIVE_EXT% -r -xr!Archive-Extension.cmd -xr!%OUTPUT_FILENAME%.zip -xr!%OUTPUT_FILENAME%.zip.txt -mx=0
 	7z a -tzip %SDEL_OPT% %OUTPUT_DIR_NAME%\%OUTPUT_FILENAME%.zip  %ARCHIVE_PREFIX%*.%ARCHIVE_EXT% -r -xr!Archive-Extension.cmd -xr!%OUTPUT_FILENAME%.zip -xr!%OUTPUT_FILENAME%.zip.txt -mx=0
 	7z l %OUTPUT_DIR_NAME%\%OUTPUT_FILENAME%.zip >%OUTPUT_DIR_NAME%\%OUTPUT_FILENAME%.zip.txt
 	TYPE %OUTPUT_DIR_NAME%\%OUTPUT_FILENAME%.zip.txt
@@ -58,15 +59,22 @@ REM ======================================================================
 
 :EXECUTE_ARCHIVE_EXT_SETTINGS_FROM_FILE
 	SET EXT_LIST_FILE=%1
+	IF NOT EXIST "%TARGET_DIR%" (
+		ECHO %TARGET_DIR% not exist.
+		PAUSE
+		EXIT
+	)
 	CD %TARGET_DIR%
 	SET OUTPUT_DIR=%OUTPUT_DIR_NAME%
 	FOR /F "skip=1 tokens=1,2,3 delims=;" %%C IN (%EXT_LIST_FILE%) DO (
 		ECHO %%C %%D %%E
 		IF %%C==1 (
 			IF NOT EXIST %OUTPUT_DIR% MD %OUTPUT_DIR%
+			ECHO 7z a -tzip %SDEL_OPT% %OUTPUT_DIR%\%%D %%E -r -xr!Archive-Extension.cmd -x!%OUTPUT_DIR_NAME% -xr!%%D -xr!%%D.txt -xr!.ts -mx=0
 			7z a -tzip %SDEL_OPT% %OUTPUT_DIR%\%%D %%E -r -xr!Archive-Extension.cmd -x!%OUTPUT_DIR_NAME% -xr!%%D -xr!%%D.txt -xr!.ts -mx=0
 			7z l %OUTPUT_DIR%\%%D >%OUTPUT_DIR%\%%D.txt
 			TYPE %OUTPUT_DIR%\%%D.txt
+			ECHO;
 		)
 	)
 	EXIT /B
